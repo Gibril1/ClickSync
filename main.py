8e26e6da34d8ad8b8c7479d7d283d95e888125a2
Gibril Idrisu Issaka <gibrilissakaiddrisu.com>
2024-06-15 21:16:17 +0000

    fixed requests

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def root():
     return "Fast API server is running"
 
 @app.post('/mediboard', status_code=status.HTTP_200_OK)
-def mediboard_events(event:Request):
+async def mediboard_events(req:Request):
+    event = await req.json()
     print(event)
 
