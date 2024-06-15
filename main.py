0f630771417ebad5f599e3ccfae7b2fcfce001ac
Gibril Idrisu Issaka <gibrilissakaiddrisu.com>
2024-06-15 21:10:32 +0000

    fixed endpoint

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 def root():
     return "Fast API server is running"
 
-@app.webhooks.post('/mediboard', status_code=status.HTTP_200_OK)
+@app.post('/mediboard', status_code=status.HTTP_200_OK)
 def mediboard_events(event:Request):
     print(event)
 
