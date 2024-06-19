20392271c110c29a07ecab42678088b3eda2b9e5
Gibril Idrisu Issaka <gibrilissakaiddrisu.com>
2024-06-19 13:48:00 +0000

    lifespan handler updated

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 env_variables = {
     "CLICKUP_API_KEY" : os.getenv( "CLICKUP_API_KEY"),
-    "TEAM_ID" : os.getenv("TEAM_ID")
+    "TEAM_ID" : os.getenv("TEAM_ID"),
+    "LIST_ID" : os.getenv("LIST_ID"),
+
 }
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
-from routes import webhook_router
+from contextlib import asynccontextmanager
+from routes.webhook_routes import webhook_router
+from routes.scheduler_routes import schedular_router
+from mongodb import get_database
+from pymongo import ASCENDING
+
+async def create_unique_index():
+    db = await get_database()
+    await db["events"].create_index([("event_id", ASCENDING)], unique=True)
+    await db["task_activities"].create_index([("task_id", ASCENDING)], unique=True)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_unique_index()
+    yield
 
 # Init app
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 # register routes
 app.include_router(webhook_router)
+app.include_router(schedular_router)
 
 
 @app.get('/')
